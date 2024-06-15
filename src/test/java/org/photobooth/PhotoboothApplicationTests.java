e76b1aae7a6e75099263719bcd9c039825ea39e1
tonnyBryan <126952328+tonnyBryan@users.noreply.github.com>
2024-06-15 19:25:27 +0300

    client
    nanis date de naissance

---
+++ src/test/java/org/photobooth/PhotoboothApplicationTests.java
@@ -0,0 +1,13 @@
+package org.photobooth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoboothApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
